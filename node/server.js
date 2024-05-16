e477509f8579f7eb847d1d4c823a6eee0a7f20d8
Barum Niazi <96373839+Barum-Niazi@users.noreply.github.com>
2024-05-16 21:19:20 +0500

    changes

--- node/server.js
+++ node/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const server = express();
+const mongoose = require("mongoose");
 
 const router = require("./routes/index.js");
+mongoose
+    .connect("mongodb://localhost:27017/gamestore")
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("Error connecting to MongoDB", err);
+    });
 
 server.listen(3000);
 
