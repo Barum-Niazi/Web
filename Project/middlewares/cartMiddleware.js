8c056d13b6ed70278ebc724b299efbe6a96269b8
Barum Niazi <96373839+Barum-Niazi@users.noreply.github.com>
2024-05-23 03:48:46 +0500

    some more cart stuff

--- Project/middlewares/cartMiddleware.js
+++ Project/middlewares/cartMiddleware.js
@@ -1,4 +1,7 @@
+const Game = require("../models/Game");
+
 module.exports = function (req, res, next) {
     res.locals.cart = req.cookies.cart ? JSON.parse(req.cookies.cart) : [];
+
     next();
 };
--- Project/routes/site/cart.js
+++ Project/routes/site/cart.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const Game = require("../../models/Game");
 const router = express.Router();
 
-router.get("/cart", (req, res) => {
-    res.render("cart", {});
+router.get("/cart", async (req, res) => {
+    try {
+        const cartItems = res.locals.cart;
+        const gamePromises = cartItems.map(async (item) => {
+            return await Game.findOne({ name: item });
+        });
+
+        const games = await Promise.all(gamePromises);
+        const filteredGames = games.filter((game) => game !== null); // Filter out any null values
+
+        res.render("cart", { cart: filteredGames });
+    } catch (err) {
+        console.error("Error fetching cart items:", err);
+        res.status(500).send("Server Error");
+    }
 });
 
 module.exports = router;
